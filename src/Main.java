44f6b6d220ee4788fde6757595805cdfa63487ad
glugg23 <m.leonhardt424@gmail.com>
2019-01-28 14:43:11 +0000

    Added naming support

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
+        System.out.print("Enter your adventurer's name: ");
+        String name = in.nextLine();
+
+        System.out.println("Welcome adventurer " + name + ", I hope you survive the brutal challenge that awaits!");
+
+        in.close();
     }
 }
