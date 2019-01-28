a4c11a92d2bf517d216669bd55b4ff181ac9c334
glugg23 <m.leonhardt424@gmail.com>
2019-01-28 14:47:45 +0000

    Basic plot setup

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,20 @@ public class Main {
 
         System.out.println("Welcome adventurer " + name + ", I hope you survive the brutal challenge that awaits!");
 
+        System.out.print("There is a tall and foreboding castle in front of you, do you enter the (c)astle or (r)un away? ");
+        String input = in.nextLine();
+
+        if(input.equals("c")) {
+            System.out.println("TODO: castle");
+
+        } else if(input.equals("r")) {
+            System.out.println("TODO: Happy end");
+
+        } else {
+            System.out.println("Please answer the prompt correctly next time.");
+            System.out.println("YOU LOSE!!");
+        }
+
         in.close();
     }
 }
